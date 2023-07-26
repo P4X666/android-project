66d6c2f70dbbc1de778cc55ac6e339e74baeb9b2
pengshixing <2644378911@qq.com>
2023-07-26 22:19:16 +0800

    添加静态图

--- covid-19/src/main/java/com/example/covid_19/MainActivity.java
+++ covid-19/src/main/java/com/example/covid_19/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ImageView flags = findViewById(R.id.flag);
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+                CountryData.countryNames));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                flags.setImageResource(CountryData.countryFlag[spinner.getSelectedItemPosition()]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        View stat = findViewById(R.id.stat);
+        stat.setOnClickListener(view -> {
+            Intent i = new Intent(this, Statistics.class);
+            startActivity(i);
+        });
     }
 }
