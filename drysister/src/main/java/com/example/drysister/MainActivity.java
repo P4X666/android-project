d0de8182340c5a0c2ac03a27e7f3f7b8e4feaf2c
pengshixing <2644378911@qq.com>
2023-09-03 22:01:17 +0800

    添加换成，使用了google的DiskLruCache.java

--- drysister/src/main/java/com/example/drysister/MainActivity.java
+++ drysister/src/main/java/com/example/drysister/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.example.drysister.api.SisterApi;
 import com.example.drysister.bean.Sister;
+import com.example.drysister.helper.SisterLoader;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
@@ -29,12 +30,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textView;
     private String TAG = "MainActivity sister";
 
+    private SisterLoader mLoader;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loader = new PictureLoader();
         textView = findViewById(R.id.pictureNum);
         executorService = Executors.newFixedThreadPool(1);
+        mLoader = SisterLoader.getInstance(MainActivity.this);
         initData();
         initUI();
     }
@@ -55,7 +59,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
         if(id == R.id.btn_show){
             if(data.get(curPos) != null){
-                loader.load(showImg, data.get(curPos).getUrl());
+//                loader.load(showImg, data.get(curPos).getUrl());
+                mLoader.bindBitmap(data.get(curPos).getUrl(),showImg,400,400);
             }
             curPos++;
             textView.setText("这是第" + curPos + "张图片");
